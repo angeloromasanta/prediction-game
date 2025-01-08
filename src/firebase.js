b3dbed1fa5899e9e9749b0c93cb91c7d87b291e9
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2025-01-08 20:06:36 +0100

    First implementation

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBm6d2YWjfmjboPvHu46QzaMTNhi8SIzgQ',
-  authDomain: 'prediction-m.firebaseapp.com',
-  projectId: 'prediction-m',
-  storageBucket: 'prediction-m.firebasestorage.app',
-  messagingSenderId: '880033693590',
-  appId: '1:880033693590:web:4a1603e0e03ebb735ba7b7',
-  measurementId: 'G-4EJG5CFHER',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID 
 };
 
 // Initialize Firebase
