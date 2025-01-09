186c8dec4ac3b8510ea647392e51be9477d37643
Angelo Romasanta <angelokenneth.romasanta@esade.edu>
2025-01-09 13:23:35 +0100

    Questions

--- src/questions.js
+++ src/questions.js
@@ -2,14 +2,57 @@ export const questions = [
   {
     id: 1,
     question:
-      "In 2028, what's the probability (%) that an AI will be able to generate a full high-quality movie from a prompt?",
-    answer: 42,
+      "Will an AI solve any important mathematical conjecture before January 1st, 2030?",
+    answer: 80,
   },
   {
     id: 2,
     question:
-      "What's the probability (%) that humans will land on Mars by 2030?",
-    answer: 35,
+      "In 2028, will an AI be able to generate a full high-quality movie to a prompt?",
+    answer: 43,
+  },
+  {
+    id: 3,
+    question:
+      "Will there be a song created by an AI system in the year-end top 100 Billboards chart by 2026?",
+    answer: 52,
+  },
+  {
+    id: 4,
+    question:
+      "Will a book written by a language model make the NY Times Best Seller list before 2030?",
+    answer: 65,
+  },
+
+  {
+    id: 5,
+    question:
+      "Will a large language model beat a super grandmaster playing chess by 2028?",
+    answer: 69,
+  },
+  {
+    id: 6,
+    question:
+      "Will we get AGI before 2030?",
+    answer: 57
+  },
+  {
+    id: 7,
+    question:
+      "Will there be a positive transition to a world with radically smarter-than-human artificial intelligence?",
+    answer: 51,
+  },
+  {
+    id: 8,
+    question:
+      "Will AI cause mass unemployment... by 2030 ? (>25% unemployement)",
+    answer: 20
+  },
+  {
+    id: 9,
+    question:
+      "Before 2032, will we see an event precipitated by AI malfunction that causes at least 100 deaths or $1B in economic damage?",
+    answer: 80
   },
   // Add more questions
 ];
