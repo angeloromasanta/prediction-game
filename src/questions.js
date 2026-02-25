895bc579d34b9b4cdb8c8e41c7c39d5a24097381
unknown <angeloromasanta@gmail.com>
2026-02-25 23:07:07 +0100

    Questions update

--- src/questions.js
+++ src/questions.js
@@ -9,31 +9,31 @@ export const questions = [
     id: 2,
     question:
       "In 2028, will at least 1/1000 of Americans be talking weekly to an AI romantic partner?",
-    answer: 86,
+    answer: 88,
   },
   {
     id: 3,
     question:
       "By 2030, will a reliable household robot be available?",
-    answer: 50,
+    answer: 55,
   },
   {
     id: 4,
     question:
-      "Will any song on Billboard Hot 100 before 2026 be composed by AI?",
-    answer: 24,
+      "In 2028, will an AI be able to play randomly selected computer games at human level without getting to practice?",
+    answer: 49,
   },
     {
     id: 5,
     question:
       "Before 2030, will AI cause US unemployment to exceed 10%?",
-    answer: 19
+    answer: 21
   },
 
   {
     id: 6,
     question:
-      "Before 2032, will AI NOT cause at least 100 deaths or $1B in economic damage?",
+      "Before 2032, AI will NOT cause more than 100 deaths or $1B in economic damage.",
     answer: 20
   },
   // Add more questions
